fde08ddccfcf740095d1cc3c4fdd10933433fa34
parkmyengbae <gambler1541@gmail.com>
2020-06-08 11:48:26 +0900

    javascript code

---
+++ html_608/background.js
@@ -0,0 +1,18 @@
+		function over(obj){
+				obj.style.background="yellow";
+			}
+		function out(obj){
+				obj.style.background="white";
+			}
+		function over2(obj){
+				obj.style.background="#cccc66";
+			}
+		function out2(obj){
+				obj.style.background="#aa6699";
+			}
+		function over3(obj){
+				obj.style.background = "#ccccff";
+				}
+		function out3(obj){
+				obj.style.background ="#ffcccc";
+				}
